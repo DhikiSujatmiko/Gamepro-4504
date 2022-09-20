fdd382a5562903ba858ac8f27809fd3aeb8aa63f
Dhiki Sujatmiko <91623783+DhikiSujatmiko@users.noreply.github.com>
2022-09-20 13:33:15 +0700

    Add files via upload

---
+++ Pertemuan 2/Debug.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Debug : MonoBehaviour
+{
+    public string Dhiki;
+    // Start is called before the first frame update
+    void Start()
+    {
+       UnityEngine.Debug.Log("Hallo There"+Dhiki);  
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
